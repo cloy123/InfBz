f5b9d165bec20721224c5d4b29af4d7a5f97e2df
cloy123 <cosoygnoy@gmail.com>
2023-02-27 21:40:30 +0300

    2 лаба

---
+++ src/main/kotlin/lab2.kt
@@ -0,0 +1,72 @@
+class lab2 {
+    companion object {
+        fun start() {
+            var command = -1
+            while (command != 3) {
+                println("Шифровать строку - 1, дешифровать - 2, выйти - 3")
+                command = readLine()!!.toInt()
+                when (command) {
+                    1 -> {
+                        print("message: ")
+                        val message = readLine()!!
+                        print("key: ")
+                        val key = readLine()!!
+                        code(message, key)
+                    }
+
+                    2 -> {
+                        print("message: ")
+                        val message = readLine()!!
+                        print("key: ")
+                        val key = readLine()!!
+                        decode(message, key)
+                    }
+                }
+            }
+        }
+
+        //это сообщение нужно закодировать
+        //абвгдежзийклмнопрстуфхцчшщъыьэюя
+        fun code(message: String, key: String){
+            val newMessage = message.replace("\\s".toRegex(), "")//удаляю пробелы из сообщения
+            val table = "абвгдежзийклмнопрстуфхцшщъыьэюя".toList()
+
+            var codeMessage = ""
+            val tableLastIndex = table.size - 1
+
+            var i = 0
+            while (i < newMessage.length){
+                var j = 0;
+                while (j < key.length && i < newMessage.length){
+                    var pos = table.indexOf(key[j]) + table.indexOf(newMessage[i])
+                    pos -= (pos / tableLastIndex)*table.size
+                    codeMessage += table[pos]
+                    i += 1
+                    j += 1
+                }
+            }
+            println(codeMessage)
+        }
+
+        fun decode(message: String, key: String){
+            val newMessage = message.replace("\\s".toRegex(), "")//удаляю пробелы из сообщения
+            val table = "абвгдежзийклмнопрстуфхцшщъыьэюя".toList()
+
+            var decodeMessage = ""
+            val tableLastIndex = table.size - 1
+
+            var i = 0
+            while (i < newMessage.length){
+                var j = 0;
+                while (j < key.length && i < newMessage.length){
+                    var pos = table.indexOf(newMessage[i]) - table.indexOf(key[j]) + table.size
+                    pos -= (pos / tableLastIndex)*table.size
+                    decodeMessage += table[pos]
+                    i += 1
+                    j += 1
+                }
+            }
+            println(decodeMessage)
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,62 +1,49 @@
 fun main(args: Array<String>) {
-    //lab1.start()
-    var command = -1
-    while (command != 3) {
-        println("Шифровать строку - 1, дешифровать - 2, выйти - 3")
-        command = readLine()!!.toInt()
-        when (command) {
-            1 -> {
-                print("message: ")
-                val message = readLine()!!
-                print("key: ")
-                val key = readLine()!!
-                code(message, key)
-            }
-
-            2 -> {
-                print("message: ")
-                val message = readLine()!!
-                print("key: ")
-                val key = readLine()!!
-                decode(message, key)
-            }
-        }
-    }
+    lab2.start()
+//    var command = -1
+//    while (command != 3) {
+//        println("Шифровать строку - 1, дешифровать - 2, выйти - 3")
+//        command = readLine()!!.toInt()
+//        when (command) {
+//            1 -> {
+//                print("message: ")
+//                val message = readLine()!!
+//                print("key: ")
+//                val key = readLine()!!
+//                code(message, key)
+//            }
+//
+//            2 -> {
+//                print("message: ")
+//                val message = readLine()!!
+//                print("key: ")
+//                val key = readLine()!!
+//                decode(message, key)
+//            }
+//        }
+//    }
 }
 
 fun code(message: String, key: String){
-    val newMessage = " " + message.replace("\\s".toRegex(), "")//удаляю пробелы из сообщения
-    var table = " абвгдежзийклмнопрстуфхцчшщъыьэюя".toList()
-    var keyTable = ArrayList<Int>()
-    keyTable.add(0)
-    for(c in key){
-        keyTable.add(table.indexOf(c))
-    }
+    val newMessage = message.replace("\\s".toRegex(), "")//удаляю пробелы из сообщения
+    val table = "абвгдежзийклмнопрстуфхцшщъыьэюя".toList()
 
-    var keyIndex = 0
     var codeMessage = ""
-    var tableLastIndex = table.size - 1
+    val tableLastIndex = table.size - 1
 
-    var i = 1
+    var i = 0
     while (i < newMessage.length){
-        for (j in 1 until keyTable.size){
-            if (i >= newMessage.length){
-                continue
-            }
-            var pos = keyTable[j] + table.indexOf(newMessage[i])
-            if(pos >= tableLastIndex){
-                pos -= tableLastIndex
-            }
+        var j = 0;
+        while (j < key.length && i < newMessage.length){
+            var pos = table.indexOf(key[j]) + table.indexOf(newMessage[i])
+            pos -= (pos / tableLastIndex)*table.size
             codeMessage += table[pos]
             i += 1
+            j += 1
         }
     }
-
-    println(codeMessage)
-
     println(codeMessage)
 }
-
 
 fun decode(message: String, key: String){
 
